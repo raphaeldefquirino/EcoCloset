6a965e4ee7853af7be7a0d62c906c3a81f14ef25
Arthur Bergamaco Alves <arthurbergamaco.alves@gmail.com>
2023-05-31 20:24:02 -0300

    Criação da página usuário.
    Criação da página e tabela do usuário.

---
+++ usuário.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabela deletar</title>
+    
+    <div class="tabela-usu">
+        <table class="table-usu">
+            <thead>
+                <tr>
+                    <th scope="col">Nome do produto</th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col">Valor</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col">...</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td data-label="Nome do produto">Produto 1</td>
+                    <td data-label="Descrição">Descrição do Produto 1</td>
+                    <td data-label="Valor">R$10.00</td>
+                    <td data-label="Telefone">123-456-7890</td>
+                    <td data-label="..."><button class="deletar">&#10006</button></td>
+                </tr>
+                <tr>
+                    <td data-label="Nome do produto">Produto 2</td>
+                    <td data-label="Descrição">Descrição do Produto 2</td>
+                    <td data-label="Valor">R$20.00</td>
+                    <td data-label="Telefone">987-654-3210</td>
+                    <td data-label="..."><button class="deletar">&#10006</button></td>
+                </tr>
+               
+            </tbody>
+        </table>
+    </div>
+    
+</body>
+</html>
