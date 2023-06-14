178a7d7399de039fdec8a23c557e56ce1d725bbe
raphaeldefquirino <124401397+raphaeldefquirino@users.noreply.github.com>
2023-06-14 19:10:37 -0300

    banco de dados bala

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php 
 
 define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', '');
-define('DB', 'ecocloset');
+define('USUARIO', 'u413309708_raphael');
+define('SENHA', 'Informaticos@2023');
+define('DB', 'u413309708_ecocloset');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possivel conectar ao banco de dados');
 
