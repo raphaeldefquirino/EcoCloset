0516e54ac91749e184d93f06d08b678535a9e68a
raphaeldefquirino <124401397+raphaeldefquirino@users.noreply.github.com>
2023-08-31 20:07:15 -0300

    autualizaçãoi

--- produto.php
+++ produto.php
@@ -1,7 +1,7 @@
 
 <?php 
 session_start();
-include ('verifica-login.php');
+include ('includes/verifica-login.php');
 ?>
 
 <!DOCTYPE html>
--- usuario.php
+++ usuario.php
@@ -1,6 +1,6 @@
     <?php 
     session_start();
-    include ('verifica-login.php');
+    include ('includes/verifica-login.php');
     ?>
 
     <!DOCTYPE html>
